7e9ab59d6ba5b8b12a2ca536986f9401f7786ad1
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-10-25 10:28:59 -0400

    Creacion de paginas de lista de libros y detalles de libros

--- DjangoPractice/locallibrary/catalog/urls.py
+++ DjangoPractice/locallibrary/catalog/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- DjangoPractice/locallibrary/catalog/views.py
+++ DjangoPractice/locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre, Language
 
@@ -21,3 +22,11 @@ def index(request):
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genres': num_genres, 'num_books_with_word': num_books_with_word}
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
